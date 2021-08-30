317a407d49782def77aa50f73f5eee525e1dcbd4
Deepak Sharma <ds19987@gmail.com>
2021-08-30 09:32:30 +0530

    new migration file

---
+++ db/db/migrations/20210829183825_fourth.js
@@ -0,0 +1,17 @@
+const tableNames = require('../../../src/constants/tableNames');
+const orderedTableNames = require('../../../src/constants/orderedTableNames');
+const {
+    addDefaultColumns,
+    createNameTable,
+    url,
+    email,
+    references,
+} = require('../../../src/lib/tableUtils');
+
+exports.up = function(knex) {
+  
+};
+
+exports.down = function(knex) {
+  
+};
