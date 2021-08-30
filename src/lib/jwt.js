ab1d95afd2de3598b067bf13e922c036057aadea
Deepak Sharma <ds19987@gmail.com>
2021-08-30 16:49:43 +0530

    added authentication middleware on routes for authorization

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { errorHandler } = require('../middlewares');
 
 function sign(payload) {
     return new Promise((resolve, reject) => {
@@ -11,6 +12,30 @@ function sign(payload) {
     })
 }
 
+function verify(req, res, next) {
+    try {
+        if(req.headers['authorization']){
+            const token = req.headers['authorization'].split(" ");
+            jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+                if(error){
+                    let error = new Error();
+                    error.message = "Invalid token";
+                    throw error;
+                }
+                next(decoded);
+            })
+        }
+        else{
+            let error = new Error();
+            error.message = "Unauthorized"
+            throw error;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
 module.exports = {
     sign,
+    verify
 }
